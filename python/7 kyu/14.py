6706f41027acc60475fe17755a618676516a3666
shajahan57 <shajahaniqbal57@gmail.com>
2025-01-23 15:25:17 +0530

    Solved a python kata

---
+++ python/7 kyu/14.py
@@ -0,0 +1,16 @@
+# https://www.codewars.com/kata/55b1fd84a24ad00b32000075/train/python
+
+# Passed
+
+def am_I_afraid(day,num):
+    afraid_logic = {
+        'Monday': lambda n: n == 12,
+        'Tuesday':  lambda n: n > 95,
+        'Wednesday': lambda n: n == 34,
+        'Thursday': lambda n: n == 0,
+        'Friday': lambda n: n % 2 == 0,
+        'Saturday': lambda n: n == 56,
+        'Sunday': lambda n: abs(n) == 666
+    }
+
+    return afraid_logic[day](num)
