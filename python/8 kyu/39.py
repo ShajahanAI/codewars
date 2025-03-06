93290c5aa2f4befbec6be3cbc65755c485bed2dc
Shajahan A I <shajahaniqbal57@gmail.com>
2025-03-06 16:48:05 +0530

    Solved a python kata

---
+++ python/8 kyu/39.py
@@ -0,0 +1,9 @@
+# https://www.codewars.com/kata/55f1b763dd670651620000ce/solutions/python
+
+# Passed
+
+def count_char_occurrences(strng, char):
+    return strng.count(char)
+
+output = count_char_occurrences("apple", "p")
+print(output)
