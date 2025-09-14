54d7f9132fc0c50b37018c7d1f774e41af0bab9f
Shajahan A I <shajahaniqbal57@gmail.com>
2025-09-14 21:45:00 +0530

    Solved a python kata

---
+++ python/8 kyu/141.py
@@ -0,0 +1,9 @@
+# https://www.codewars.com/kata/57eae20f5500ad98e50002c5/train/python
+
+# Passed
+
+def no_space(x):
+    return x.replace(" ", str())
+
+output = no_space('8 j 8   mBliB8g  imjB8B8  jl  B')
+print(output)
