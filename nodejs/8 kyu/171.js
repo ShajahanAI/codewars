3354862d8e742d43912e67c9078d93d3e5b7f607
Shajahan A I <shajahaniqbal57@gmail.com>
2025-10-31 23:39:11 +0530

    Solved a nodejs kata

---
+++ nodejs/8 kyu/171.js
@@ -0,0 +1,10 @@
+// https://www.codewars.com/kata/5ce9c1000bab0b001134f5af/train/javascript
+
+// Passed
+
+const quarterOf = (month) => {
+  return Math.ceil(month / 3);
+};
+
+const output = quarterOf(8);
+console.log(output);
