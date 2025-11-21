71c27944bb3821c6cc660a6149d4531b0dfca1bb
Shajahan A I <shajahaniqbal57@gmail.com>
2025-11-21 22:54:22 +0530

    Solved a nodejs kata

---
+++ nodejs/8 kyu/181.js
@@ -0,0 +1,10 @@
+// https://www.codewars.com/kata/52ceafd1f235ce81aa00073a/train/javascript
+
+// Passed
+
+function plural(n) {
+  return n !== 1;
+}
+
+const output = plural(25);
+console.log(output);
