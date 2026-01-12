b510d3add1443b7e2fd5e9cb77c4b3a9c65dab03
Shajahan A I <shajahaniqbal57@gmail.com>
2026-01-12 22:53:09 +0530

    Solved a nodejs kata

---
+++ nodejs/8 kyu/221.js
@@ -0,0 +1,11 @@
+// https://www.codewars.com/kata/53369039d7ab3ac506000467/train/javascript
+
+// Passed
+
+function boolToWord(bool) {
+  let result = bool ? "Yes" : "No";
+  return result;
+}
+
+const output = boolToWord(true);
+console.log(output);
