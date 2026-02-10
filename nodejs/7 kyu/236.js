5676b6d09391d6c9d74ff14cb2c6da8e413b865d
Shajahan A I <shajahaniqbal57@gmail.com>
2026-02-10 22:31:16 +0530

    Solved a nodejs kata

---
+++ nodejs/7 kyu/236.js
@@ -0,0 +1,11 @@
+// https://www.codewars.com/kata/55fd2d567d94ac3bc9000064/train/javascript
+
+// Passed
+
+function rowSumOddNumbers(n) {
+  let totalSum = n ** 3;
+  return totalSum;
+}
+
+const output = rowSumOddNumbers(42);
+console.log(output);
