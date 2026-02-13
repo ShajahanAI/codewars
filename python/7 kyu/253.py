2dcfa3b1f856afd725e58d832cf24e2faeeb69e9
Shajahan A I <shajahaniqbal57@gmail.com>
2026-02-13 23:16:02 +0530

    Solved a python kata

---
+++ python/7 kyu/253.py
@@ -0,0 +1,10 @@
+# https://www.codewars.com/kata/58fa273ca6d84c158e000052/train/python
+
+# Passed
+
+def digits(n):
+    num_digits = len(str(n))
+    return num_digits
+
+output = digits(9876543210)
+print(output)
