3331c160453506f27e67ff9c9d394ca5e9e2b53b
Shajahan A I <shajahaniqbal57@gmail.com>
2026-03-14 15:40:43 +0530

    Solved a nodejs kata

---
+++ nodejs/8 kyu/261.js
@@ -0,0 +1,11 @@
+// https://www.codewars.com/kata/5933a1f8552bc2750a0000ed/train/javascript
+
+// Passed
+
+function nthEven(n) {
+  let result = (n - 1) * 2;
+  return result;
+}
+
+const output = nthEven(1298734);
+console.log(output);
