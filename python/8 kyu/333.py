319b16ee6e98477e6bbc59f22dbb7e28e4b0f650
Shajahan A I <shajahaniqbal57@gmail.com>
2026-05-19 23:05:05 +0530

    Solved another python kata

---
+++ python/8 kyu/333.py
@@ -0,0 +1,10 @@
+# https://www.codewars.com/kata/57a5c31ce298a7e6b7000334/train/python
+
+# Passed
+
+def bin_to_decimal(inp):
+    decimal_number = int(inp, 2)
+    return decimal_number
+
+output = bin_to_decimal("101010")
+print(output)
