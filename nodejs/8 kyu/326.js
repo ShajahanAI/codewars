7444d69ea82a4b2ca77aacd677fa3c8c932e6a5e
Shajahan A I <shajahaniqbal57@gmail.com>
2026-05-21 23:47:33 +0530

    Solved a nodejs kata

---
+++ nodejs/8 kyu/326.js
@@ -0,0 +1,11 @@
+// https://www.codewars.com/kata/523b623152af8a30c6000027/train/javascript
+
+// Passed
+
+function square(num) {
+  let result = num ** 2;
+  return result;
+}
+
+const output = square(3);
+console.log(output);
