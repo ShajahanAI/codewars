0dfe33fc2ab79b4a7a9958bcb07c905bad681b54
Shajahan A I <shajahaniqbal57@gmail.com>
2026-05-27 23:12:20 +0530

    Solved another python kata

---
+++ python/8 kyu/346.py
@@ -0,0 +1,10 @@
+# https://www.codewars.com/kata/545afd0761aa4c3055001386/train/python
+
+# Passed
+
+def take(arr,n):
+    result = arr[:n]
+    return result
+
+output = take([0, 1, 2, 3, 5, 8, 13], 3)
+print(output)
