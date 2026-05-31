61a8df82f6eabbaea3e6211486acd35310bb5e04
Shajahan A I <shajahaniqbal57@gmail.com>
2026-05-31 23:38:13 +0530

    Solved a python kata

---
+++ python/7 kyu/347.py
@@ -0,0 +1,10 @@
+# https://www.codewars.com/kata/558445a88826e1376b000011/train/python
+
+# Passed
+
+def age(x, y):    
+    result = (x * y) / (y - 1)
+    return result
+
+output = age(6, 3)
+print(output)
