9e7553f0dfcac63c1a1ed3671594bdc7a22028df
Shajahan A I <shajahaniqbal57@gmail.com>
2026-06-10 23:58:44 +0530

    Solved a nodejs kata

---
+++ nodejs/8 kyu/343.js
@@ -0,0 +1,11 @@
+// https://www.codewars.com/kata/57e76bc428d6fbc2d500036d/train/javascript
+
+// Passed
+
+function stringToArray(string) {
+  let words = string.split(" ");
+  return words;
+}
+
+const output = stringToArray("I love arrays they are my favorite");
+console.log(output);
