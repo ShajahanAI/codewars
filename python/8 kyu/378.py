99fd1af0cd7f46a6759bf15e40094bed7bfe11f5
Shajahan A I <shajahaniqbal57@gmail.com>
2026-06-25 23:54:56 +0530

    Solved a python kata

---
+++ python/8 kyu/378.py
@@ -0,0 +1,10 @@
+# https://www.codewars.com/kata/53dc23c68a0c93699800041d/train/python
+
+# Passed
+
+def smash(words):
+    result = " ".join(words)
+    return result
+
+output = smash(["hello", "amazing", "world"])
+print(output)
