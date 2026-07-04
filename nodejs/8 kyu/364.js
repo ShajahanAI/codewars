2a94bf8aa545196955f8c0b4da0fd56d7594f9c7
Shajahan A I <shajahaniqbal57@gmail.com>
2026-07-04 23:07:12 +0530

    Solved a nodejs kata

---
+++ nodejs/8 kyu/364.js
@@ -0,0 +1,11 @@
+// https://www.codewars.com/kata/55acfc59c3c23d230f00006d/train/javascript
+
+// Passed
+
+function getASCII(c) {
+  let result = c.charCodeAt();
+  return result;
+}
+
+const output = getASCII("A");
+console.log(output);
