02356ca06bf0b614a37c051b0d26c5fa45d3ebdf
Shajahan A I <shajahaniqbal57@gmail.com>
2026-07-20 23:01:10 +0530

    Solved another nodejs kata

---
+++ nodejs/8 kyu/392.js
@@ -0,0 +1,11 @@
+// https://www.codewars.com/kata/55a70521798b14d4750000a4/train/javascript
+
+// Passed
+
+function greet(name) {
+  let result = `Hello, ${name} how are you doing today?`;
+  return result;
+}
+
+const output = greet("Ryan");
+console.log(output);
