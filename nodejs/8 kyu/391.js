3158abbd519a8acaeb00d4761832a1156b923367
Shajahan A I <shajahaniqbal57@gmail.com>
2026-07-20 22:57:58 +0530

    Solved another nodejs kata

---
+++ nodejs/8 kyu/391.js
@@ -0,0 +1,11 @@
+// https://www.codewars.com/kata/5265326f5fda8eb1160004c8/train/javascript
+
+// Passed
+
+function numberToString(num) {
+  let result = String(num);
+  return result;
+}
+
+const output = numberToString(67);
+console.log(output);
