5dde66d8416a94c5afea5a3ccfed763338451032
Shajahan A I <shajahaniqbal57@gmail.com>
2026-08-31 23:08:44 +0530

    Solved a nodejs kata

---
+++ nodejs/7 kyu/422.js
@@ -0,0 +1,11 @@
+// https://www.codewars.com/kata/53d2697b7152a5e13d000b82/train/javascript
+
+// Passed
+
+function copyList(l) {
+  let result = [...l];
+  return result;
+}
+
+const output = copyList([1, 2, 3, 4]);
+console.log(output);
