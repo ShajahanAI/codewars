02499dececed039ae45c88abb47198377eb8d64e
Shajahan A I <shajahaniqbal57@gmail.com>
2026-08-31 23:30:11 +0530

    Solved a nodejs kata

---
+++ nodejs/8 kyu/423.js
@@ -0,0 +1,12 @@
+// https://www.codewars.com/kata/55cb632c1a5d7b3ad0000145/train/javascript
+
+// Passed
+
+function hoopCount(n) {
+  let result =
+    n >= 10 ? "Great, now move on to tricks" : "Keep at it until you get it";
+  return result;
+}
+
+const output = hoopCount(11);
+console.log(output);
