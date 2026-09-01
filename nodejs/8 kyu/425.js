03a27409a6296fd07458c55185fa734efb2eb961
Shajahan A I <shajahaniqbal57@gmail.com>
2026-09-01 22:56:14 +0530

    Solved another nodejs kata

---
+++ nodejs/8 kyu/425.js
@@ -0,0 +1,11 @@
+// https://www.codewars.com/kata/56e2f59fb2ed128081001328/train/javascript
+
+// Passed
+
+function printArray(array) {
+  let result = array.join();
+  return result;
+}
+
+const output = printArray([2, 4, 5, 2]);
+console.log(output);
