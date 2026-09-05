1ee59e813681387176ab843dcd2f5d8f8d3f9555
Shajahan A I <shajahaniqbal57@gmail.com>
2026-09-05 23:59:04 +0530

    Solved another python kata

---
+++ python/8 kyu/455.py
@@ -0,0 +1,10 @@
+# https://www.codewars.com/kata/55ad04714f0b468e8200001c/train/python
+
+# Passed
+
+def get_char(c):
+    result = chr(c)
+    return result
+
+output = get_char(33)
+print(output)
